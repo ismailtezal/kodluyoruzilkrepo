67a04692081e3f64f878138319288ecd8362f208
İsmail Tezal <ismailtezal33@gmail.com>
2023-03-06 10:59:01 +0300

    Added new variable for two data objects

--- reactPractice/App.js
+++ reactPractice/App.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 const getData = async (userId) => {
-  const { data: userData } = await axios(
-    `https://jsonplaceholder.typicode.com/users/${userId}`
-  );
-  const { data: userPostData } = await axios(
-    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
-  );
-  console.log("USER:", userData);
-  console.log("POSTS:", userPostData);
+  const API_URL = 'https://jsonplaceholder.typicode.com';
+  const userData = await axios.get(`${API_URL}/users/${userId}`);
+  const postData = await axios.get(`${API_URL}/posts?userId=${userId}`);
+
+  const obj = {
+    ...userData.data,
+    posts: postData.data,
+  };
+  console.log(obj);
 };
 
 export default getData;
